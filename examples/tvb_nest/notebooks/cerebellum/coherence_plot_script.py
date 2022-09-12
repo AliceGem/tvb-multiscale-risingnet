840907ed05ef5ea02086085e502f23c6713b2055
AliceGem <alice.geminiani@unipv.it>
2022-09-12 17:12:57 +0200

    Corrected coherence colors

--- examples/tvb_nest/notebooks/cerebellum/coherence_plot_script.py
+++ examples/tvb_nest/notebooks/cerebellum/coherence_plot_script.py
@@ -17,7 +17,7 @@ for test in test_names:
     avg_coherence[test] = np.mean(np.array([CxyR,CxyL]),axis=0)
 
 # Power Spectra and Coherence for M1 - S1 barrel field
-color = {'cosim': 'black', 'MF_cerebON': 'pink', 'MF_cerebOFF': 'red'}
+color = {'cosim': 'red', 'MF_cerebON': 'gray', 'MF_cerebOFF': 'black'}
 fig = only_plot_selected_spectra_coherence_and_diff(fR, avg_coherence, color, fmin=0.0, fmax=50.0, figsize=(15, 5))
 fig.savefig('coherence_plot_comparison.png')
 fig.savefig('coherence_plot_comparison.svg')
