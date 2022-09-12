19c34a86f3e11a1a383574c178c185a668e3bd68
AliceGem <alice.geminiani@unipv.it>
2022-09-12 15:51:07 +0200

    Coherence plot script
    Added script for coherence plot starting from corresponding notebook

---
+++ examples/tvb_nest/notebooks/cerebellum/coherence_plot_script.py
@@ -0,0 +1,23 @@
+# Import packages
+import pickle
+import numpy as np
+
+
+from examples.tvb_nest.notebooks.cerebellum.utils import  only_plot_selected_spectra_coherence_and_diff
+
+
+# Load files
+test_names = ['MF_cerebON', 'MF_cerebOFF','cosim']
+
+avg_coherence = {}
+for test in test_names:
+    file_coherence = open("coherence_"+test+"_2sec.pickle",'rb')
+    print(file_coherence)
+    [CxyR, fR, fL, CxyL]=pickle.load(file_coherence)
+    avg_coherence[test] = np.mean(np.array([CxyR,CxyL]),axis=0)
+
+# Power Spectra and Coherence for M1 - S1 barrel field
+color = {'cosim': 'black', 'MF_cerebON': 'pink', 'MF_cerebOFF': 'red'}
+fig = only_plot_selected_spectra_coherence_and_diff(fR, avg_coherence, color, fmin=0.0, fmax=50.0, figsize=(15, 5))
+fig.savefig('coherence_plot_comparison.png')
+fig.savefig('coherence_plot_comparison.svg')
